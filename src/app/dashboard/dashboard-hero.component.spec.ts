3ed143705e825b83bc045055d280c1a8b027801e
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-08-03 13:22:16 -0500

    how to test input output component ahd how to use TestHost test

--- src/app/dashboard/dashboard-hero.component.spec.ts
+++ src/app/dashboard/dashboard-hero.component.spec.ts
@@ -2,6 +2,52 @@ import { TestBed, ComponentFixture } from "@angular/core/testing"
 import {By} from '@angular/platform-browser';
 
 import {DashboardHeroComponent} from './dashboard-hero.component'
+import { Hero } from '../model/hero';
+import { click} from '../model/testing/index'
+import { Component } from '@angular/core';
+
+@Component({
+    template:`
+      <dashboard-hero
+         [hero]=hero
+         (selected)=onSelected($event)
+      >
+      </dashboard-hero>
+    `
+})
+class TestHostComponent{
+    hero: Hero = {id: 42, name: "Test Name"};
+    selectedHero: Hero;
+    onSelected(hero: Hero){
+        this.selectedHero = hero;
+    }
+
+}
+
+fdescribe("testing dashboard hero with TestHostComponent",()=>{
+    let fixture: ComponentFixture<TestHostComponent>;
+    let testHost : TestHostComponent;
+    let heroEl: any;
+    beforeEach(()=>{
+        TestBed.configureTestingModule({
+            declarations: [DashboardHeroComponent,TestHostComponent]
+        });
+        fixture = TestBed.createComponent(TestHostComponent);
+        testHost = fixture.componentInstance;
+        heroEl = fixture.nativeElement.querySelector('.hero');
+        fixture.detectChanges();
+    });
+
+    it('should display hero name',()=>{
+        const expectedPipedName = testHost.hero.name.toUpperCase();
+        expect(heroEl.textContent).toContain(expectedPipedName);
+    })
+    it("should raise selected event when clicked", ()=>{
+        click(heroEl);
+        expect(testHost.selectedHero).toEqual(testHost.hero);
+    })
+
+})
 
 fdescribe("dashboard hero block",()=>{
     let fixture: ComponentFixture<DashboardHeroComponent>;
@@ -31,5 +77,31 @@ fdescribe("dashboard hero block",()=>{
     })
     it("should display the hero name in uppercase",()=>{
         expect(heroEl.textContent).toContain(expectedHero.name.toUpperCase());
+    })
+    it("should raise selected event when clicked (trigger event handler)",()=>{
+        let selectedHero: Hero;
+        comp.selected.subscribe((hero: Hero)=>{
+           selectedHero = hero;
+        });
+        heroDe.triggerEventHandler("click",null);
+        expect(selectedHero).toEqual(expectedHero);
+    })
+
+    it("should raise selected event when clicked (element.click)",()=>{
+        let selectedHero: Hero;
+        comp.selected.subscribe((hero: Hero)=>{
+           selectedHero = hero;
+        });
+        heroEl.click();
+        expect(selectedHero).toEqual(expectedHero);
+    })
+    it("should raise selected event when clicked (click helper)",()=>{
+        let selectedHero: Hero;
+        comp.selected.subscribe((hero: Hero)=>{
+           selectedHero = hero;
+        });
+        click(heroDe);
+        click(heroEl);
+        expect(selectedHero).toEqual(expectedHero);
     })
 })
